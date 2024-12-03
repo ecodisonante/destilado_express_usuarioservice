9b46b55553c43f8bbd2b267239064395e48e1a98
Francisco Valdes <ecodisonante@gmail.com>
2024-12-03 20:50:24 -0300

    obtenerUsuarioPorId

--- src/main/java/com/destilado_express/usuarioservice/controller/UsuarioController.java
+++ src/main/java/com/destilado_express/usuarioservice/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.destilado_express.usuarioservice.controller;
 
 import com.destilado_express.usuarioservice.model.Usuario;
 import com.destilado_express.usuarioservice.service.auth.AuthService;
+import com.destilado_express.usuarioservice.service.auth.JwtService;
 import com.destilado_express.usuarioservice.service.user.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,7 @@ public class UsuarioController {
 
     @Autowired
     private AuthService authService;
-   
+
     // Obtener todos los usuarios
     @GetMapping
     public ResponseEntity<List<Usuario>> obtenerTodosLosUsuarios() {
@@ -38,7 +39,16 @@ public class UsuarioController {
 
     // Obtener usuario por ID
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Long id) {
+    public ResponseEntity<Object> obtenerUsuarioPorId(@PathVariable Long id) {
+        // Verificar si el usuario es admin
+        if (!authService.esAdmin()) {
+            Usuario usuarioAutenticado = authService.getUsuarioAutenticado();
+            if (usuarioAutenticado.getId() != id) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                        .body("No tienes permiso para ver otros usuarios");
+            }
+        }
+
         Usuario usuario = usuarioService.getUsuarioById(id);
         if (usuario != null) {
             return new ResponseEntity<>(usuario, HttpStatus.OK);
@@ -82,7 +92,6 @@ public class UsuarioController {
                 ? ResponseEntity.ok("Usuario actualizado")
                 : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
     }
-
 
     // Eliminar un usuario por ID
     @DeleteMapping("/{id}")
